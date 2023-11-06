264e08890de4270935dfe46788cef5beaa69f873
Esat <147237700+E5AT@users.noreply.github.com>
2023-11-06 13:56:52 +0200

    Create 06.11.cs

---
+++ 0Schule/06.11.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for(int i = 0; i <= 10; i++)
+                Console.WriteLine(i);
+            Console.WriteLine("Числата от 0 до 10");*/
+
+            /*int i = 0;
+            while (i < 10)
+            {
+                i++; Console.WriteLine(i);
+            }*/
+
+            /*string input = Console.ReadLine();
+            while(input!="Stop")
+            {
+                Console.WriteLine(input);
+                input = Console.ReadLine();
+            }*/
+
+            /*double x = 130.0;
+            int g = 2010;
+            while(x>10)
+            {
+                x = 0.9 * x;
+                g++;
+            }
+            Console.WriteLine(g);*/
+
+
+            /*Console.Write("a = "); int a = int.Parse(Console.ReadLine());
+            Console.Write("b = "); int b = int.Parse(Console.ReadLine());
+            if(b==0)
+            {
+                Console.WriteLine($"НОД = {a}");
+            }
+            else
+            {
+                while (b != 0)
+                {
+                    int r = a % b;
+                    a = b;
+                    b = r;
+                }
+                Console.WriteLine($"НОД = {a}");
+            }
+            Console.ReadLine();*/
+
+
+            /*int s = int.Parse(Console.ReadLine());
+            while(s>0)
+            {
+                Console.WriteLine(s);
+                s++;
+                if (s == 100) break;
+            }*/
+
+            /*int a = 0;
+            while(a<10)
+            {
+                if(a%2==0)
+                {
+                    a++;
+                    continue;
+                }
+                Console.WriteLine(a);
+                a++;
+            }*/
+
+
+        }
+    }
+}

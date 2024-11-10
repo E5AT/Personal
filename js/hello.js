f32910641b6180ed139ddb769c7e92df8bff93ac
E5AT <esatcraft@gmail.com>
2024-11-10 09:12:10 +0200

    Added js folder

---
+++ js/hello.js
@@ -0,0 +1,14 @@
+console.log("Hello world!")
+
+let a, b
+a=1
+b=2
+let sum = a+b
+let dif = a-b
+let div = a/b
+let mult = a*b
+console.log(sum, dif, div, mult)
+
+let name = "Esat"
+let age = 18
+console.log(`Your name is ${name} and you are ${age} years old.`)

461631f9368f81b5014bba2e64285153d4f021c2
E5AT <esatcraft@gmail.com>
2025-07-22 14:45:05 +0300

    started c++

---
+++ C++ projects/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    x=5;
+
+    int y=6;
+
+    int sum = x+y;
+
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << sum;
+    return 0;
+}

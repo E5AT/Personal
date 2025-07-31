e73d92c22582d6ca1c45b72726933e77bbb973c7
E5AT <esatcraft@gmail.com>
2025-07-31 15:48:54 +0300

    finished variables part

--- C++ Full Course for free ⚡️/variables.cpp
+++ C++ Full Course for free ⚡️/variables.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    int x;
+    /*int x;
     x=5;
 
     int y=6;
@@ -11,6 +11,13 @@ int main(int argc, char const *argv[])
 
     std::cout << x << "\n";
     std::cout << y << "\n";
-    std::cout << sum;
+    std::cout << sum;*/
+
+    std::string name = "Esat";
+    int age = 18;
+    double length = 1.79;
+    char gradeMath = 'A';
+    
+    std::cout << "Hi " << name << ", you're " << age << " years old and " << length << "m tall! You got " << gradeMath << " from your Math test too!";
     return 0;
 }

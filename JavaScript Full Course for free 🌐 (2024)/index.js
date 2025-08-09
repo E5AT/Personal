0d2ed07487378922d7d7a2e6372c00c5da7d8fe7
E5AT <esatcraft@gmail.com>
2025-08-09 17:08:37 +0300

    variables

--- JavaScript Full Course for free 🌐 (2024)/index.js
+++ JavaScript Full Course for free 🌐 (2024)/index.js
@@ -4,8 +4,8 @@
 //window.alert(`This is an alert!`);
 //window.alert(`I like pizza!`);
 
-document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = "I like pizza!";
+//document.getElementById("myH1").textContent = "Hello";
+//document.getElementById("myP").textContent = "I like pizza!";
 
 // This is a comment
 
@@ -15,3 +15,9 @@ document.getElementById("myP").textContent = "I like pizza!";
     a
     comment
 */
+
+//variables
+let age = 18, firstName = "Esat", online = false;
+document.getElementById("myH1").textContent = `Hi ${firstName},`;
+document.getElementById("myP1").textContent = `You're ${age} years old`;
+document.getElementById("myP2").textContent = `And you're ${online?"online":"offline"}`;
